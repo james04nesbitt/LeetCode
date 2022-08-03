93d72780ec4353b2f0c1edb7f1d5e8d83e942e1b
james04nesbitt <96902483+james04nesbitt@users.noreply.github.com>
2022-08-03 00:13:03 -0400

    Create 392:is_Subsequence.cpp

---
+++ 392:is_Subsequence.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j = 0, n = s.size();
+        for(int i = 0; i < t.size() && j <n; i++){
+            if(s[j] == t[i]) j++;
+        }
+        return j == n;
+    }
+};
